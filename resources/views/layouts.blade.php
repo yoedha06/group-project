02f1f1328844dd0d2e2b7651e62ee44facf77afa
ryan21rb <129033902+ryan21rb@users.noreply.github.com>
2024-01-15 11:33:59 +0700

    Resolve conflicts in layouts.blade.php

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -16,9 +16,7 @@
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
         integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        
     </script>
 </head>
 
