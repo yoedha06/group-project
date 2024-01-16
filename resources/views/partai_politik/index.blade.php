c9a96de1f36c84af89c2b3373ff3771afe3e8227
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-01-16 10:37:08 +0700

    .

--- resources/views/partai_politik/index.blade.php
+++ resources/views/partai_politik/index.blade.php
@@ -6,12 +6,12 @@
 
         <a href="{{ route('partai_politik.create') }}" class="btn btn-primary mb-3">Tambah Partai Politik</a>
 
-        @if(session('success'))
+        @if (session('success'))
             <div class="alert alert-success">
                 {{ session('success') }}
             </div>
         @endif
-        
+
         <table class="table">
             <thead>
                 <tr>
@@ -25,7 +25,7 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach($partaiPolitiks as $partaiPolitik)
+                @foreach ($partaiPolitiks as $partaiPolitik)
                     <tr>
                         <td>{{ $loop->iteration }}</td>
                         <td>{{ $partaiPolitik->Id_Partai }}</td>
@@ -34,13 +34,16 @@
                         <td>{{ $partaiPolitik->JumlahAnggota }}</td>
                         <td>{{ $partaiPolitik->PemimpinPartai }}</td>
                         <td>
-                        <a href="{{ route('partai_politik.edit', ['Id_Partai' => $partaiPolitik->Id_Partai]) }}" class="btn btn-warning">Edit</a>
-                            
-                        <form action="{{ route('partai_politik.delete', ['Id_Partai' => $partaiPolitik->Id_Partai]) }}" method="post" style="display:inline;">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus?')">Hapus</button>
-</form>
+                            <a href="{{ route('partai_politik.edit', ['Id_Partai' => $partaiPolitik->Id_Partai]) }}"
+                                class="btn btn-warning">Edit</a>
+
+                            <form action="{{ route('partai_politik.delete', ['Id_Partai' => $partaiPolitik->Id_Partai]) }}"
+                                method="post" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger"
+                                    onclick="return confirm('Apakah Anda yakin ingin menghapus?')">Hapus</button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
