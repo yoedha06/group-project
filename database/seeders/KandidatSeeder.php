9e13b69fdd3704e0d8b6317110184afe2b097967
Dzaki Ahmad Fauzan <dzakiahmadfa123@gmail.com>
2024-01-16 08:27:53 +0700

    seed

---
+++ database/seeders/KandidatSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class KandidatSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('kandidat')->insert([
+            [
+                'Nama_Kandidat' => 'Ganjar Pranowo',
+                'Tanggal_Lahir' => '2024-01-01',
+                'Partai_Politik' => 'PDI',
+                'Nomor_Urut' => 1008,
+                'Program_Kerja' => 'Membuka 20 Lapangan Kerja',
+            ],
+            [
+                'Nama_Kandidat' => 'Prabowo Subianto',
+                'Tanggal_Lahir' => '2024-01-02',
+                'Partai_Politik' => 'Gerinda',
+                'Nomor_Urut' => 1009,
+                'Program_Kerja' => 'Memperkuat Pertahanan Negara',
+            ],
+            [
+                'Nama_Kandidat' => 'Anies Baswedan',
+                'Tanggal_Lahir' => '2024-01-03',
+                'Partai_Politik' => 'Unaffiliated',
+                'Nomor_Urut' => 1010,
+                'Program_Kerja' => 'Gratis BBM',
+            ],
+            [
+                'Nama_Kandidat' => 'Megawati',
+                'Tanggal_Lahir' => '2024-01-13',
+                'Partai_Politik' => 'PDI',
+                'Nomor_Urut' => 1012,
+                'Program_Kerja' => 'KORUPSI',
+            ],
+        ]);
+    }
+}
