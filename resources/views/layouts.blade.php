83c5d3b7fdfc0cbe59b181036a630fe6a39b6899
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-01-17 09:34:21 +0700

    hapus coment

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -7,21 +7,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pemilu</title>
 
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
-    <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
         integrity="sha512-xwz5KD/WT06P4ATnA5ou22Ld9bpAjsEe+JykRQs4Mj47Ro9X1W9wCr/YQnNRvBwoQzN3eFiOSt5ZyZ5OL/kDgw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    {{-- script logoooooooooo --}}
     <link rel="icon" type="image/png" href="/assets/images/logo.png" />
 
-    <!-- Your Custom CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="icon" type="image/png" href="/assets/images/logo.png" /> <!-- Your Custom CSS -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
