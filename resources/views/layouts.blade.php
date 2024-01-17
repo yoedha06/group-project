5fa5bb462f5ec0c2a9ab67f9ca560d4f54e17b18
Yuda Hidayat <yuda.hidayat2005@gmail.com>
2024-01-17 09:16:14 +0700

    buat chepi

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -17,7 +17,7 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    
+    {{-- script logo --}}
     <link rel="icon" type="image/png" href="/assets/images/logo.png" />
     <!-- Your Custom CSS -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
