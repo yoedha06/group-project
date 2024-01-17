d1eda8fdf4b51cd606f2ed6441c8430c962321b4
Dzaki Ahmad Fauzan <dzakiahmadfa123@gmail.com>
2024-01-17 11:25:00 +0700

    aaaa

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -16,7 +16,7 @@
 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    {{-- link logo pemilu check--}}
+    {{-- link logo pemilu checks--}}
     <link rel="icon" type="image/png" href="/assets/images/logo.png" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
