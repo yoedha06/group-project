75d389b14a3a6dc011de6e1018d0a9561b2ebc53
Yuda Hidayat <yuda.hidayat2005@gmail.com>
2024-01-17 13:04:53 +0700

    nyoba conflik

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- Name Title --}}
-    <title>Pemilu</title>
+    <title>Pemiluu</title>
 
 
     {{-- Link Bootstapppp --}}
