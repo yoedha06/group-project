1b338371c053f9b6f2bd3d7daf825a02e7cdef90
Yuda Hidayat <yuda.hidayat2005@gmail.com>
2024-01-17 08:51:02 +0700

    tambah logo loading

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -18,7 +18,7 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     
-
+    <link rel="icon" type="image/png" href="/assets/images/logo.png" />
     <!-- Your Custom CSS -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
