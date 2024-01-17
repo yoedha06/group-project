f225cc937555a625e220b91031f04a33fbbfe416
Yuda Hidayat <yuda.hidayat2005@gmail.com>
2024-01-17 10:50:05 +0700

    logo pemilu

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -16,7 +16,7 @@
 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    {{-- link logo --}}
+    {{-- link logo pemilu --}}
     <link rel="icon" type="image/png" href="/assets/images/logo.png" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
