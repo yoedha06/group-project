e073321a01df50e9df8cc06033d8cd2e9af83ca0
Dzaki Ahmad Fauzan <dzakiahmadfa123@gmail.com>
2024-01-19 11:21:23 +0700

    cepilobagaya

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -101,12 +101,11 @@
 
     <div class="container-fluid d-flex flex-column justify-content-center align-items-center">
         <div class="card">
-            <!-- Your card content goes here -->
+
         </div>
     </div>
 
 
-    <!-- Include the necessary JavaScript libraries -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
