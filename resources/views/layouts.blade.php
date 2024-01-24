0f37b7a19105a7a191c5893095bc5dc95007b4a6
Dzaki Ahmad Fauzan <dzakiahmadfa123@gmail.com>
2024-01-24 08:55:09 +0700

    splashscreen

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -27,62 +27,104 @@
     <link rel="icon" type="image/png" href="/assets/images/logo.png" />
     <!-- Link CSS -->
     <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
+
 </head>
 {{-- Bagian body --}}
 
 <body>
+    <style>
+        #splash-screen {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            z-index: 1000;
+            /* Pastikan di atas elemen lain */
+        }
 
-    <lord-icon src="https://cdn.lordicon.com/kthelypq.json" trigger="hover"
-        style="width:250px;height:250px;cursor:pointer;" id="myLordIcon">
-    </lord-icon>
+        #splash-content {
+            text-align: center;
+        }
+
+        #myLordIcon {
+            width: 250px;
+            height: 250px;
+            cursor: pointer;
+        }
+    </style>
     </head>
 
-                    <body>
-                        <div class="wrapper">
-                            <!-- Sidebar -->
-                            <nav id="sidebar">
-                                <div class="sidebar-header">
-                                    <img src="{{ asset('/assets/images/ppp-removebg-preview.png') }}" alt="Logo">
-                                    <h3>Pemilu</h3>
-                                </div>
-                                <ul class="list-unstyled">
-                                    <li class="{{ Request::is('pemilih*') ? 'active' : '' }}">
-                                        <a href="{{ route('pemilih.index') }}"><i class="bi bi-person"></i> Pemilih</a>
-                                    </li>
-                                    <li class="{{ Request::is('kandidat*') ? 'active' : '' }}">
-                                        <a href="{{ route('kandidat.index') }}"><i class="bi bi-person"></i>
-                                            Kandidat</a>
-                                    </li>
-                                    <li class="{{ Request::is('partai_politik*') ? 'active' : '' }}">
-                                        <a href="{{ route('partai_politik.index') }}"><i class="bi bi-building"></i>
-                                            Partai Politik</a>
-                                    </li>
-                                    <li class="{{ Request::is('hasilpemilihan*') ? 'active' : '' }}">
-                                        <a href="{{ route('hasilpemilihan.index') }}"><i class="bi bi-bar-chart"></i>
-                                            Hasil
-                                            Pemilihan</a>
-                                    </li>
-                                    @if (!request()->has('keyword'))
-                                        <a href="{{ route('dashboard') }}" class="btn btn-danger btn-block mt-4"><i
-                                                class="bi bi-arrow-left-circle"></i> Back to Dashboard</a>
-                                    @endif
-                                </ul>
-                            </nav>
+    <body>
+        <!-- Splash Screen -->
+        <div id="splash-screen">
+            <div id="splash-content">
+                <!-- Ganti konten lord-icon dengan gambar -->
+                <img src="https://3.bp.blogspot.com/-GWNKq7YnVtg/XJiG5McwawI/AAAAAAAAB2k/zz11WE6_aSgPbIZpkGOyy0FA2DSWRigvQCLcBGAs/s1600/logo-kotak-pemilu%2B.png"
+                    alt="Logo" style="width: 250px; height: 250px;">
+                <h2>Pemilu...</h2>
+            </div>
+        </div>
 
-                            <div class="container mt-5">
-                                {{-- content all --}}
-                                @yield('content')
+        <body>
+            <div class="wrapper">
+                <!-- Sidebar -->
+                <nav id="sidebar">
+                    <div class="sidebar-header">
+                        <img src="{{ asset('/assets/images/ppp-removebg-preview.png') }}" alt="Logo">
+                        <h3>Pemilu</h3>
+                    </div>
+                    <ul class="list-unstyled">
+                        <li class="{{ Request::is('pemilih*') ? 'active' : '' }}">
+                            <a href="{{ route('pemilih.index') }}"><i class="bi bi-person"></i> Pemilih</a>
+                        </li>
+                        <li class="{{ Request::is('kandidat*') ? 'active' : '' }}">
+                            <a href="{{ route('kandidat.index') }}"><i class="bi bi-person"></i>
+                                Kandidat</a>
+                        </li>
+                        <li class="{{ Request::is('partai_politik*') ? 'active' : '' }}">
+                            <a href="{{ route('partai_politik.index') }}"><i class="bi bi-building"></i>
+                                Partai Politik</a>
+                        </li>
+                        <li class="{{ Request::is('hasilpemilihan*') ? 'active' : '' }}">
+                            <a href="{{ route('hasilpemilihan.index') }}"><i class="bi bi-bar-chart"></i>
+                                Hasil
+                                Pemilihan</a>
+                        </li>
+                        @if (!request()->has('keyword'))
+                            <a href="{{ route('dashboard') }}" class="btn btn-danger btn-block mt-4"><i
+                                    class="bi bi-arrow-left-circle"></i> Back to Dashboard</a>
+                        @endif
+                    </ul>
+                </nav>
 
-                            </div>
-                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-                                integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
-                            </script>
-                            <!-- LInk Js -->
-                            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-                            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-                    </body>
-                    {{-- Penutup Body --}}
+                <div class="container mt-5">
+                    {{-- content all --}}
+                    @yield('content')
+
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+                </script>
+                <!-- LInk Js -->
+                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+                <script>
+                    // Menunggu konten utama dimuat
+                    window.addEventListener('load', function() {
+                        // Menghilangkan splash screen setelah 2 detik
+                        setTimeout(function() {
+                            document.getElementById('splash-screen').style.display = 'none';
+                        }, 2000);
+                    });
+                </script>
+        </body>
+        {{-- Penutup Body --}}
 
 
 </html>
