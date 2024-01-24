09ea39811300d688aca3930f0d352b5b1a551ecd
Dzaki Ahmad Fauzan <dzakiahmadfa123@gmail.com>
2024-01-24 10:35:51 +0700

    loading jekk

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -125,7 +125,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <!-- Splash Screen -->
+        <!-- Splash Screeenn -->
         <div id="splash-screen">
             <div id="splash-content">
                 <!-- Ganti konten lord-icon dengan gambar GIF -->
