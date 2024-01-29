c9c34046777dc3fa592e683ba89736742e2915bc
ryan21rb <baktiryan182@gmail.com>
2024-01-29 16:46:30 +0700

    RB

--- resources/views/pemilih/edit.blade.php
+++ resources/views/pemilih/edit.blade.php
@@ -4,14 +4,16 @@
 
 <html lang="en">
 
-    <head>
+<head>
+
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Edit Pemilih</title>
-        <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
         <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    </head>
+
+</head>
 
     <body>
         <div class="container">
@@ -61,8 +63,9 @@
                                 <input type="text" name="koordinat" id="koordinat"
                                     class="form-control {{ $errors->has('koordinat') ? 'is-invalid' : '' }}"
                                     value="{{ $pemilih->koordinat }}" readonly>
-                                <div id="map" style="height: 70vh; margin-top:5px;"></div>
+
                             </div>
+                            <div id="map" style="height: 400px; width: 100%;"></div>
                             <div class="form-group">
                                 <label for="no_ktp">no KTP:</label>
                                 <input type="text" name="no_ktp" id="no_ktp" class="form-control {{ $errors->has('no_ktp') ? 'is-invalid' : '' }}"
@@ -92,77 +95,40 @@
                         </form>
                     </div>
                 </div>
+                <div id="map" style="height: 400px; width: 100%;"></div>
+
             </div>
         </div>
-    </div>
 
-    <!-- Sesuaikan dengan library JavaScript yang Anda gunakan, contoh menggunakan Bootstrap JS -->
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-<script>
-    var map = L.map('map').setView([-2.5489, 118.0149], 5);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
-    var marker = L.marker([-2.5489, 118.0149], { draggable: true }).addTo(map);
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
 
-    map.on('move', function (e) {
-        var newLatLng = map.getCenter();
-        marker.setLatLng(newLatLng);
-        updateCoordinateInput(newLatLng.lat, newLatLng.lng);
-    });
 
-    marker.on('dragend', function (e) {
-        var newLatLng = marker.getLatLng();
-        map.setView(newLatLng);
-        updateCoordinateInput(newLatLng.lat, newLatLng.lng);
-    });
+            var map = L.map('map').setView([-6.9147, 107.6098], 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-    function updateCoordinateInput(lat, lng) {
-        document.getElementById('koordinat').value = lat + ', ' + lng;
-    }
+            // Tambahkan marker di tengah-tengah peta sebagai petunjuk
+            var centerMarker = L.marker(map.getCenter(), { draggable: false }).addTo(map);
 
-    function showAddressOnMap(address) {
-        geocodeAddress(address, function(coordinates) {
-            if (coordinates) {
-                var newLatLng = L.latLng(coordinates[0], coordinates[1]);
-                map.setView(newLatLng);
-                marker.setLatLng(newLatLng);
+            // Event listener untuk menangani perubahan tampilan peta setelah digeser oleh pengguna
+            map.on('move', function() {
+                var newLatLng = map.getCenter();
+                centerMarker.setLatLng(newLatLng);
                 updateCoordinateInput(newLatLng.lat, newLatLng.lng);
-            } else {
-                console.error('Geocoding failed for address: ' + address);
+            });
+
+            function updateCoordinateInput(lat, lng) {
+                document.getElementById('koordinat').value = lat + ', ' + lng;
             }
         });
-    }
+    </script>
 
-    // Panggil fungsi untuk menampilkan alamat pada peta (gunakan alamat dari data)
-    @if(isset($pemilih->alamat))
-        showAddressOnMap('{{ $pemilih->alamat }}');
-    @endif
 
-    function geocodeAddress(address, callback) {
-        var geocodingUrl = 'https://nominatim.openstreetmap.org/search?format=json&limit=1&q=' + encodeURIComponent(address);
-
-        fetch(geocodingUrl)
-            .then(response => response.json())
-            .then(data => {
-                if (data.length > 0) {
-                    var location = data[0];
-                    var coordinates = [location.lat, location.lon];
-                    callback(coordinates);
-                } else {
-                    console.error('Geocoding failed for address: ' + address);
-                    callback(null);
-                }
-            })
-            .catch(error => {
-                console.error('Error during geocoding: ', error);
-                callback(null);
-            });
-    }
-</script>
 </html>
 @endsection
--- resources/views/pemilih/tambah.blade.php
+++ resources/views/pemilih/tambah.blade.php
@@ -1,14 +1,9 @@
 @extends('layouts')
-<<<<<<< HEAD
-    <title>Tambah Pemilih</title>
-=======
 <title>Tambah Pemilih</title>
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
 <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
->>>>>>> aba5fc11a9a98ea25d290a8037f2dada7c8e4f00
 @section('content')
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -21,7 +16,9 @@
 
                             <div class="form-group">
                                 <label for="nama_pemilih">Nama Pemilih:</label>
-                                <input type="text" name="nama_pemilih" id="nama_pemilih" class="form-control {{ $errors->has('nama_pemilih') ? 'is-invalid' : ''}}" value="{{ old('nama_pemilih') }}">
+                                <input type="text" name="nama_pemilih" id="nama_pemilih"
+                                    class="form-control {{ $errors->has('nama_pemilih') ? 'is-invalid' : '' }}"
+                                    value="{{ old('nama_pemilih') }}">
                                 @error('nama_pemilih')
                                     <div class="invalid-feedback">
                                         {{ $message }}
@@ -32,17 +29,18 @@
                             <br>
                             <div class="form-group">
                                 <label for="tanggal_lahir">Tanggal Lahir</label>
-                                <input type="date" name="tanggal_lahir" id="tanggal_lahir" class="form-control {{ $errors->has('tanggal_lahir') ? 'is-invalid' : ''}}" value="{{ old('tanggal_lahir') }}">
-                            @error('tanggal_lahir')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                            @enderror
+                                <input type="date" name="tanggal_lahir" id="tanggal_lahir"
+                                    class="form-control {{ $errors->has('tanggal_lahir') ? 'is-invalid' : '' }}"
+                                    value="{{ old('tanggal_lahir') }}">
+                                @error('tanggal_lahir')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <br>
                             <div class="form-group">
                                 <label for="alamat">Alamat:</label>
-
                                 <input type="text" name="alamat" id="alamat"
                                     class="form-control {{ $errors->has('alamat') ? 'is-invalid' : '' }}"
                                     value="{{ old('alamat') }}">
@@ -55,11 +53,15 @@
                             <br>
                             {{-- //koordinat --}}
                             <div class="form-group">
-                                <label for="koordinat">Koordinat:</label>
-                                <input type="text" name="koordinat" id="koordinat"
-                                    class="form-control {{ $errors->has('koordinat') ? 'is-invalid' : '' }}"
-                                    value="{{ old('koordinat') }}">
-
+                                <label for="koordinat">Koordinat</label>
+                                <div id="map" style="height: 300px; margin-top: 10px;"></div>
+                                <input type="text" name="koordinat" id="koordinat" class="form-control {{ $errors->has('koordinat') ? 'is-invalid' : '' }}" value="{{ old('koordinat') }}">
+                                @error('koordinat')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
                             <script>
                                 var map = L.map('map').setView([-6.9147, 107.6098], 13);
 
@@ -98,24 +100,32 @@
                             <br>
                             <div class="form-group">
                                 <label for="no_ktp">No KTP:</label>
-                                <input type="number" name="no_ktp" id="no_ktp" class="form-control {{ $errors->has('no_ktp') ? 'is-invalid' : ''}}" value="{{ old('no_ktp') }}">
-                            @error('no_ktp')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
+                                <input type="number" name="no_ktp" id="no_ktp"
+                                    class="form-control {{ $errors->has('no_ktp') ? 'is-invalid' : '' }}"
+                                    value="{{ old('no_ktp') }}">
+                                @error('no_ktp')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                             <br>
                             <div class="form-group">
                                 <label for="status_pemilihan">Status Pemilihan:</label>
-                                <select name="status_pemilihan" class="form-control {{ $errors->has('status_pemilihan') ? 'is-invalid' : ''}}" id="status_pemilihan" >
+                                <select name="status_pemilihan"
+                                    class="form-control {{ $errors->has('status_pemilihan') ? 'is-invalid' : '' }}"
+                                    id="status_pemilihan">
                                     <option value="" selected disabled>Pilih Status</option>
-                                    <option value="Belum Memilih" {{ old('status_pemilihan') == 'Belum Memilih' ? 'selected' : '' }}>Belum Memilih</option>
-                                    <option value="Sudah Memilih" {{ old('status_pemilihan') == 'Sudah Memilih' ? 'selected' : '' }}>Sudah Memilih</option>
+                                    <option value="Belum Memilih"
+                                        {{ old('status_pemilihan') == 'Belum Memilih' ? 'selected' : '' }}>Belum Memilih
+                                    </option>
+                                    <option value="Sudah Memilih"
+                                        {{ old('status_pemilihan') == 'Sudah Memilih' ? 'selected' : '' }}>Sudah Memilih
+                                    </option>
                                 </select>
-                            @error('status_pemilihan')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                            @enderror
+                                @error('status_pemilihan')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <br>
                             <div class="text-center">
