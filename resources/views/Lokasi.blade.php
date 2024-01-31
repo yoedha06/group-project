185e71df84efe5644219607178382b6a24bdfb15
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-01-31 10:23:23 +0700

    .

--- resources/views/Lokasi.blade.php
+++ resources/views/Lokasi.blade.php
@@ -7,10 +7,6 @@
         <title>Peta</title>
         <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
         <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-        <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
-        <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
-        <script src="https://unpkg.com/@geoman-io/leaflet-history-control"></script>
-
     </head>
 
     <body>
@@ -65,6 +61,47 @@
             .map-controls button:hover {
                 opacity: 0.8;
             }
+
+            .map-controls div label {
+                display: flex;
+                align-items: center;
+                margin-right: 15px;
+                cursor: pointer;
+            }
+
+            .map-controls div label input {
+                margin-right: 8px;
+            }
+
+            .map-controls div label span {
+                font-size: 16px;
+            }
+
+            .map-controls div label:hover {
+                text-decoration: underline;
+            }
+
+            #validationMessage {
+                display: none;
+                margin-top: 10px;
+                padding: 10px;
+                border: 2px solid #d9534f;
+                /* Warna border untuk pesan error */
+                border-radius: 5px;
+                color: #d9534f;
+                /* Warna teks untuk pesan error */
+                background-color: #f2dede;
+                /* Warna latar belakang untuk pesan error */
+            }
+
+            #validationMessage.success {
+                border-color: #5bc0de;
+                /* Warna border untuk pesan sukses */
+                color: #5bc0de;
+                /* Warna teks untuk pesan sukses */
+                background-color: #d9edf7;
+                /* Warna latar belakang untuk pesan sukses */
+            }
         </style>
         <div class="map-container">
             <div class="map-controls">
@@ -79,121 +116,118 @@
                 </div>
             </div>
 
+            <div id="validationMessage" style="display: none; color: red; margin-top: 10px;"></div>
+            <br>
             <div id="map" style="height: 600px"></div>
+
 
             {{-- script js maps --}}
             <script>
                 var pemilih = {!! json_encode($pemilih) !!};
                 var map = L.map('map').setView([-6.895364793103795, 107.53971757412086], 13);
 
-                var coordinates = pemilih.map(function(p) {
-                    var coords = p.koordinat.split(',').map(parseFloat);
-                    return new L.LatLng(coords[0], coords[1]);
-                });
-
-                // var polyline = L.polyline(coordinates, {
-                //     color: 'blue'
-                // }).addTo(map);
-
                 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(map);
+
                 var markers = [];
                 var currentFilter = ''; // Variabel untuk menyimpan status pemilihan yang sedang di-filter
+
 
                 function calculateBounds() {
                     var bounds = new L.LatLngBounds();
 
-                    function addMarker(p) {
+                    markers.forEach(function(m) {
+                        bounds.extend(m.marker.getLatLng());
+                    });
+
+                    return bounds;
+                }
+
+                function setupMap() {
+
+                    var maxBounds = [
+                        [-10, 95], // Koordinat sudut barat daya (SW)
+                        [5, 150] // Koordinat sudut timur laut (NE)
+                    ];
+
+                    map = L.map('map', {
+                        center: [-2.5489, 118.0149],
+                        zoom: 5,
+                        maxBounds: maxBounds, // Mengatur batas maksimum peta
+                        maxBoundsViscosity: 0.9, // Kontrol kekenyalan ketika melampaui batas
+                        dragging: true, // Aktifkan fungsi geser peta
+                    });
+
+                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    }).addTo(map);
+
+                    pemilih.forEach(function(p) {
+                        addMarker(p);
+                    });
+
+                    // Setelah menambahkan semua marker, atur tampilan peta agar mencakup semua marker
+                    var bounds = calculateBounds();
+                    map.fitBounds(bounds);
+                }
+
+                function addMarker(p) {
+                    if (p.nama_pemilih && p.status_pemilihan && p.koordinat) {
                         var coordinates = p.koordinat.split(',').map(function(coord) {
                             return parseFloat(coord.trim());
                         });
 
-                        var markerColor = p.status_pemilihan === 'Sudah Memilih' ? 'green' : 'red';
+                        // Validasi koordinat
+                        if (!isNaN(coordinates[0]) && !isNaN(coordinates[1])) {
+                            var markerColor = p.status_pemilihan === 'Sudah Memilih' ? 'green' : 'red';
 
-                        var marker = L.marker(coordinates, {
-                            icon: L.divIcon({
-                                className: 'custom-marker',
-                                html: '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">' +
-                                    '<path d="M12 0C5.37 0 0 5.37 0 12s12 24 12 24 12-10.8 12-24S18.63 0 12 0zm0 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z" fill="' +
-                                    markerColor + '"/></svg>',
-                                iconSize: [15, 15],
-                                iconAnchor: [15, 15],
-                            })
-                        }).addTo(map);
+                            var marker = L.marker(coordinates, {
+                                icon: L.divIcon({
+                                    className: 'custom-marker',
+                                    html: '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">' +
+                                        '<path d="M12 0C5.37 0 0 5.37 0 12s12 24 12 24 12-10.8 12-24S18.63 0 12 0zm0 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z" fill="' +
+                                        markerColor + '"/></svg>',
+                                    iconSize: [15, 15],
+                                    iconAnchor: [15, 15],
+                                })
+                            }).addTo(map);
 
-                        var popupContent = "<b>Nama Pemilih:</b> " + p.nama_pemilih + "<br><b>Status Pemilihan:</b> " + p
-                            .status_pemilihan + "<br><b>Koordinat:</b> " + p.koordinat;
-                        marker.bindPopup(popupContent).openPopup();
+                            var popupContent = "<b>Nama Pemilih:</b> " + p.nama_pemilih + "<br><b>Status Pemilihan:</b> " + p
+                                .status_pemilihan +
+                                "<br><b>Koordinat:</b> " + p.koordinat;
+                            marker.bindPopup(popupContent).openPopup();
 
-                        markers.push({
-                            marker,
-                            status: p.status_pemilihan,
-                            nama: p.nama_pemilih
-                        });
-                    }
-
-                    var map = L.map('map').setView([51.505, -0.09], 13);
-                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        maxZoom: 19,
-                    }).addTo(map);
-
-                    var historyControl = new L.HistoryControl();
-                    map.addControl(historyControl);
-
-                    var routingControl = L.Routing.control({
-                        waypoints: coordinates.map(function(coord) {
-                            return L.latLng(coord.lat, coord.lng);
-                        }),
-                        createMarker: function(i, wp, nWps) {
-                            if (i === 0 || i === nWps - 1) {
-                                return L.marker(wp.latLng, {
-                                    icon: L.divIcon({
-                                        className: 'custom-marker',
-                                        html: '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">' +
-                                            '<path d="M12 0C5.37 0 0 5.37 0 12s12 24 12 24 12-10.8 12-24S18.63 0 12 0zm0 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z" fill="' +
-                                            (wp.options.status === 'Sudah Memilih' ? 'green' : 'red') +
-                                            '"/></svg>',
-                                        iconSize: [15, 15],
-                                        iconAnchor: [15, 15],
-                                    })
-                                });
-                            } else {
-                                return null;
-                            }
-                        },
-                        lineOptions: {
-                            styles: [{
-                                color: 'green',
-                                opacity: 1,
-                                weight: 3.5
-                            }]
-                        }
-                    }).addTo(map);
-
-                    function filterMarkers(status) {
-                        currentFilter = status;
-                        var filteredMarkers = markers.filter(function(m) {
-                            return m.status === status;
-                        });
-
-                        // Menampilkan atau menyembunyikan pesan validasi
-                        var validationMessage = document.getElementById('validationMessage');
-                        if (filteredMarkers.length === 0) {
-                            validationMessage.innerText = "Data Tidak Ditemukan.";
-                            validationMessage.style.color = 'red';
-                            validationMessage.style.display = 'block';
+                            markers.push({
+                                marker,
+                                status: p.status_pemilihan,
+                                nama: p.nama_pemilih
+                            });
                         } else {
-                            validationMessage.style.display = 'none';
+                            console.warn("Data koordinat tidak valid untuk pemilih:", p);
                         }
+                    } else {
+                        console.warn("Data pemilih tidak lengkap atau tidak valid:", p);
                     }
-                    // Menambahkan atau menghapus marker sesuai filter
-                    pemilih.forEach(function(p) {
-                        addMarker(p);
+                }
+
+                pemilih.forEach(function(p) {
+                    addMarker(p);
+                });
+
+                function filterMarkers(status) {
+                    currentFilter = status;
+                    markers.forEach(function(m) {
+                        if (m.status === status) {
+                            m.marker.addTo(map);
+                        } else {
+                            map.removeLayer(m.marker);
+                        }
                     });
                 }
 
                 function resetMarkers() {
+                    currentFilter = ''; // Reset variabel currentFilter
                     markers.forEach(function(m) {
                         m.marker.addTo(map);
                     });
@@ -207,11 +241,13 @@
                     var searchValue = document.getElementById('searchInput').value.toLowerCase();
                     var searchDataFound = false;
                     var isDataSelected = false;
+
                     markers.forEach(function(m) {
                         if ((m.nama.toLowerCase().includes(searchValue) || searchValue === '') &&
                             (currentFilter === '' || m.status === currentFilter)) {
                             m.marker.addTo(map);
                             searchDataFound = true; // Setel ke true jika ada data yang ditemukan
+
                             if (m.status === 'Sudah Memilih') {
                                 isDataSelected = true; // Setel ke true jika ada data yang sudah memilih
                             }
@@ -223,79 +259,12 @@
                             map.removeLayer(m.marker);
                         }
                     });
-                }
-                // Menampilkan atau menyembunyikan pesan validasi
-                var validationMessage = document.getElementById('validationMessage');
-                if (!searchDataFound) {
-                    validationMessage.innerText = "Data Tidak Ditemukan.";
-                    validationMessage.style.color = 'red';
-                    validationMessage.style.display = 'block';
-                } else {}
 
-                validationMessage.innerText = "Data Ditemukan!";
-                validationMessage.style.color = 'green';
-                validationMessage.style.display = 'block';
 
-                // Sembunyikan pesan validasi jika ada data yang ditemukan
-                document.getElementById('validationMessage').style.display = 'none';
-
-                // Tampilkan pesan validasi bahwa data ditemukan
-                document.getElementById('validationMessage').innerText = "Data ditemukan!";
-                document.getElementById('validationMessage').style.color = 'green';
-                document.getElementById('validationMessage').style.display = 'block';
-
-                function filterMarkers(status) {
-                    markers.forEach(function(m) {
-                        if (m.status === status) {
-                            m.marker.addTo(map);
-                        } else {
-                            map.removeLayer(m.marker);
-                        }
-                    });
-                }
-
-                function resetMarkers() {
-                    markers.forEach(function(m) {
-                        m.marker.addTo(map);
-                    });
-                }
-
-                function showLocationOnMap(latitude, longitude) {
-                    map.setView([latitude, longitude], 13);
-                }
-
-                function searchByName() {
-                    var searchValue = document.getElementById('searchInput').value.toLowerCase();
-                    var searchDataFound = false;
-                    var isDataSelected = false;
-                    markers.forEach(function(m) {
-                        if ((m.nama.toLowerCase().includes(searchValue) || searchValue === '') &&
-                            (currentFilter === '' || m.status === currentFilter)) {
-                            m.marker.addTo(map);
-                            searchDataFound = true; // Setel ke true jika ada data yang ditemukan
-                            if (m.status === 'Sudah Memilih') {
-                                isDataSelected = true; // Setel ke true jika ada data yang sudah memilih
-                            }
-                            var coordinates = m.marker.getLatLng();
-                            map.flyTo(coordinates, 17, {
-                                duration: 2 // Anda dapat menyesuaikan durasi (dalam detik) sesuai kebutuhan
-                            });
-                        } else {
-                            map.removeLayer(m.marker);
-                        }
-                    });
-                    // Menampilkan atau menyembunyikan pesan validasi
-                    var validationMessage = document.getElementById('validationMessage');
                     if (!searchDataFound) {
-                        validationMessage.innerText = "Data Tidak Ditemukan.";
-                        validationMessage.style.color = 'red';
-                        validationMessage.style.display = 'block';
+                        document.getElementById('validationMessage').innerText = "Data Belum Memilih.";
+                        document.getElementById('validationMessage').style.display = 'block';
                     } else {
-
-                        validationMessage.innerText = "Data Ditemukan!";
-                        validationMessage.style.color = 'green';
-                        validationMessage.style.display = 'block';
-
                         // Sembunyikan pesan validasi jika ada data yang ditemukan
                         document.getElementById('validationMessage').style.display = 'none';
 
@@ -303,7 +272,6 @@
                         document.getElementById('validationMessage').innerText = "Data ditemukan!";
                         document.getElementById('validationMessage').style.color = 'green';
                         document.getElementById('validationMessage').style.display = 'block';
-
                     }
                 }
 
@@ -313,18 +281,29 @@
                     var filterDataFound = false; // Tandai apakah data sesuai dengan filter
 
                     markers.forEach(function(m) {
-                        if (m.nama.toLowerCase().includes(searchValue)) {
+                        if (m.status === status) {
                             m.marker.addTo(map);
+                            filterDataFound = true; // Setel ke true jika ada data yang sesuai dengan filter
                         } else {
                             map.removeLayer(m.marker);
                         }
                     });
+
+                    // Tampilkan pesan validasi jika tidak ada data yang sesuai dengan filter
+                    if (!searchDataFound) {
+                        document.getElementById('validationMessage').innerText = "Data Belum Memilih.";
+                        document.getElementById('validationMessage').style.display = 'block';
+                    } else {
+                        // Sembunyikan pesan validasi jika ada data yang sesuai dengan filter
+                        document.getElementById('validationMessage').style.display = 'none';
+
+                        document.getElementById('validationMessage').innerText = "Data ditemukan!";
+                        document.getElementById('validationMessage').style.color = 'green';
+                        document.getElementById('validationMessage').style.display = 'block';
+                    }
                 }
             </script>
-
-        </div>
     </body>
-
 
     </html>
 @endsection
