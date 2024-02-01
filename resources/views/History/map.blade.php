0456eaaec352e899d21fc7377e36e0c99860bc76
Dzaki Ahmad Fauzan <dzakiahmadfa123@gmail.com>
2024-02-01 11:50:24 +0700

    basil

--- resources/views/History/map.blade.php
+++ resources/views/History/map.blade.php
@@ -19,12 +19,8 @@
 
         var historyData = @json($history);
 
-        var map = L.map('map');
-
         var layerGroup = L.layerGroup();
         var polylinePoints = [];
-
-        // ...
 
         for (var i = 0; i < historyData.length; i++) {
             var latlngStr = historyData[i].latlng;
@@ -32,6 +28,7 @@
             var lat = parseFloat(latlngArr[0]);
             var lng = parseFloat(latlngArr[1]);
             var speed = parseFloat(historyData[i].speeds);
+            var accuracy = parseFloat(historyData[i].accuracy);
 
             var color;
             if (speed < 20) {
@@ -48,66 +45,29 @@
                 stroke: false,
             });
 
-
             layerGroup.addLayer(circleMarker);
 
             polylinePoints.push([lat, lng]);
+
+            // Calculate polyline color and weight based on accuracy
             var polylineColor = speed < 20 ? "green" : speed >= 20 && speed <= 40 ? "yellow" : "red";
+            var polylineWeight = accuracy === 0 ? 10 : accuracy > 20 ? 2 : 5;
 
             if (polylinePoints.length > 1) {
                 // Draw polyline segment with appropriate color and weight
-                L.polyline(polylinePoints.slice(-2), {
+                var polyline = L.polyline(polylinePoints.slice(-2), {
                     color: polylineColor,
-                    weight: 5,
+                    weight: polylineWeight,
                     opacity: 1.0, // Sesuaikan dengan kebutuhan Anda
                 }).addTo(map);
+
+                // Add popup with data
+                var popupContent = "Speed: " + speed + " km/h<br>Accuracy: " + accuracy + " m";
+                polyline.bindPopup(popupContent);
             }
         }
 
-        // ...
-
-
-
         // Adding a Polyline connecting the circle markers
         layerGroup.addTo(map);
-            // Create a popup with latitude and bounds information
-            var popupContent = "<b>Latitude:</b> " + lat +
-                "<br><b>Longitude:</b> " + lng +
-                "<br><b>Bounds:</b> " + "{{ $record->bounds }}";
-
-            marker.bindPopup(popupContent);
-        @endforeach
-
-        // Add the polyline after adding the markers
-        var coordinatesArray = [
-            @foreach ($history as $record)
-                [{{ $record->latlng }}],
-            @endforeach
-        ];
-
-        // Define a function to determine the color based on speed
-        function getColor(speeds) {
-            return speeds < 40 ? 'green' : speeds < 60 ? 'yellow' : 'red';
-        }
-
-        var polyline = L.polyline(coordinatesArray, {
-            color: getColor({{ $history[0]->speeds }}), // Initial color based on the first record's speed
-        }).addTo(map);
-        
-        //BOUNDS
-        var bounds = L.latLngBounds(coordinatesArray);
-        map.fitBounds(bounds);  // Set MaxBounds agar tidak bisa di-zoom keluar dari garis batas
-
-        // Tambahkan layer tile OpenStreetMap
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        // Set view agar peta berada di tengah batas
-        map.fitBounds(bounds);
-        // Update polyline color based on speed dynamically
-        @foreach ($history as $record)
-            polyline.setStyle({ color: getColor({{ $record->speeds }}) });
-        @endforeach
     </script>
 @endsection
