03e8d37805ae382c474647f303c575a82e81b120
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-02-01 16:24:54 +0700

    .

--- resources/views/History/map.blade.php
+++ resources/views/History/map.blade.php
@@ -39,7 +39,8 @@
 </head>
 
 @section('content')
-<button class="btn btn-danger" onclick="window.location.href='{{ route('history.index') }}'"><i class="bi bi-arrow-left-circle"></i>&nbsp;Kembali ke History</button>
+    <button class="btn btn-danger" onclick="window.location.href='{{ route('history.index') }}'"><i
+            class="bi bi-arrow-left-circle"></i>&nbsp;Kembali ke History</button>
     <div id="map" style="height: 600px"></div>
     <script>
         var map = L.map('map').setView([-6.895364793103795, 107.53971757412086], 13);
@@ -107,22 +108,19 @@
             }
         }
 
-<<<<<<< HEAD
         // Adding a Polyline connecting the circle markers
         layerGroup.addTo(map);
-=======
-                var allLatLngs = polylinePoints.concat(historyData.map(function(item) {
-                var latlngStr = item.latlng;
-                var latlngArr = latlngStr.split(", ");
-                var lat = parseFloat(latlngArr[0]);
-                var lng = parseFloat(latlngArr[1]);
-                return L.latLng(lat, lng);
-            }));
+        var allLatLngs = polylinePoints.concat(historyData.map(function(item) {
+            var latlngStr = item.latlng;
+            var latlngArr = latlngStr.split(", ");
+            var lat = parseFloat(latlngArr[0]);
+            var lng = parseFloat(latlngArr[1]);
+            return L.latLng(lat, lng);
+        }));
 
-            map.fitBounds(L.latLngBounds(allLatLngs));
+        map.fitBounds(L.latLngBounds(allLatLngs));
 
-            // Adding a Polyline connecting the circle markers
-            layerGroup.addTo(map);
->>>>>>> 576f39006cb19f550f1ad5da1936b92dbdf2443b
+        // Adding a Polyline connecting the circle markers
+        layerGroup.addTo(map);
     </script>
 @endsection
